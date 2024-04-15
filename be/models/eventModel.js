a18b4af58fdc935610ee4d437620de22a6b4047c
Mhien862 <104289231+Mhien862@users.noreply.github.com>
2024-04-15 13:06:20 +0700

    fix main

---
+++ be/models/eventModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const eventSchema = mongoose.Schema({
+  eventName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstClosureDate: Date,
+  finalClosureDate: Date,
+  faculty: {
+    type: String,
+    ref: "Faculty",
+    required: true,
+  },
+  academicYear: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "AcademicYear",
+    required: true,
+  },
+  contributions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Contribution",
+      required: true,
+    },
+  ],
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+export default Event;
