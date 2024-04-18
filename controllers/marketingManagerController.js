8f9a03ea73f7ab5ed801dbf43a04aa3cf963f1da
taylortien-99 <taylortien99@gmail.com>
2024-04-18 17:33:54 +0700

    update 1

--- controllers/marketingManagerController.js
+++ controllers/marketingManagerController.js
@@ -59,9 +59,9 @@ const getContribution = async (req, res) => {
 const getContributionImg = async (req, res) => {
   let image = req.params.name;
   const __dirname = path.resolve();
-  let filePath = path.join(__dirname, `be/uploads/${image}`);
+  let filePath = path.join(__dirname, `uploads/${image}`);
   res.sendFile(filePath);
-}
+};
 
 const getDashboardStatistics = async (req, res) => {
   try {
@@ -104,4 +104,9 @@ const getDashboardStatistics = async (req, res) => {
     res.status(500).json({ message: "Failed to get dashboard statistics" });
   }
 };
-export { downloadAllFiles, getContribution, getDashboardStatistics, getContributionImg };
+export {
+  downloadAllFiles,
+  getContribution,
+  getDashboardStatistics,
+  getContributionImg,
+};
