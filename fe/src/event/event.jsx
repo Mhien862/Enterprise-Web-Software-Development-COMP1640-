feb95f91b00b87c4771899f4123b6ab01fc3cfd9
blueoc-quyendoan <quyen.doan@blueoc.tech>
2024-04-18 09:04:57 +0700

    feat: create edit event - chart

--- fe/src/event/event.jsx
+++ fe/src/event/event.jsx
@@ -1,24 +1,200 @@
 import React, { useEffect, useState } from "react";
-import { Button, Col, Row, Select, Space, Table, notification } from "antd";
+import { 
+  Button, 
+  Col, 
+  Row, 
+  Select, 
+  Space, 
+  Table, 
+  notification,
+  Form, 
+  Input, 
+  DatePicker,
+} from "antd";
 import axiosInstance from "../services/axios.service";
-import { DeleteOutlined, EditOutlined, EyeOutlined } from "@ant-design/icons";
+import { createEventAPI, editEventAPI } from "../services/EventService";
+import { DeleteOutlined, EditOutlined, EyeOutlined, CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import Search from "antd/es/input/Search";
 import { CheckCircleOutlined, WarningOutlined } from "@ant-design/icons";
 
 const Event = () => {
+  const [events, setEvents] = useState([]);
+  const [pagination, setPagination] = useState({});
+
+  const navigate = useNavigate();
+
+  const [form] = Form.useForm();
+  const [isCreate, setIsCreate] = useState(false);
+  const [editId, setEditId] = useState('');
+  const [isRefetch, setIsRefetch] = useState(false)
+
+  const handleCreate = () => {
+    setEditId('')
+    setIsCreate(true)
+    const formInput = (children, dataIndex) => 
+    (<Form.Item
+      name={dataIndex}
+      style={{
+        margin: 0,
+      }}
+      rules={[
+        {
+          required: true,
+          message: `Please Input ${dataIndex}!`,
+        },
+      ]}>
+        {children}
+    </Form.Item>)
+    const newData = {
+      eventName: formInput(<Input style={{width: 80}}/>, 'eventName'),
+      firstClosureDate: formInput(<DatePicker style={{width: 160}} />, 'firstClosureDate'),
+      finalClosureDate:  formInput(<DatePicker style={{width: 160}} />, 'finalClosureDate'),
+      academicYear: formInput(<Input style={{width: 160}}/>, 'academicYear'),
+      faculty: formInput(<Input style={{width: 80}}/>, 'faculty'),
+      isCreate: true,
+    }
+    setEvents([newData, ...events])
+  }
+
+  const handleEdit = (record) => {
+    handleRefetch()
+    form.setFieldsValue({
+      ...record,
+      firstClosureDate: '', // datepicker input is string not timestamp  
+      finalClosureDate:  '', // datepicker input is string not timestamp 
+    });
+    setEditId(record._id)
+  }
+
+  const handleRefetch = () => {
+    form.setFieldsValue({
+      eventName: '',
+      academicYear: '',
+      faculty: '',
+      firstClosureDate: '',
+      finalClosureDate:  '',
+    });
+    setIsCreate(false)
+    setEditId('')
+  }
+
+  const handleRemove = (index) => {
+    if(!editId) {
+      setEvents((pre) => {
+        pre.splice(index, 1)
+        return [...pre]
+      })
+    }
+    handleRefetch()
+  }
+
+  const handleSave = async (isCreate, id) => {
+    const newData = await form.validateFields();
+    if (!!isCreate) {
+      try {
+        await createEventAPI(newData)
+        notification.open({
+          message: "Create Success",
+          icon: (
+            <CheckCircleOutlined
+              style={{
+                color: "#00ff66",
+              }}
+            />
+          ),
+        });
+        setIsRefetch(true)
+      } catch (error) {
+        notification.open({
+          message: error.response.data.message,
+          icon: (
+            <WarningOutlined
+              style={{
+                color: "#e91010",
+              }}
+            />
+          ),
+        });
+      }
+    } else {
+      try {
+        await editEventAPI({id, ...newData})
+        notification.open({
+          message: "Edit Success",
+          icon: (
+            <CheckCircleOutlined
+              style={{
+                color: "#00ff66",
+              }}
+            />
+          ),
+        });
+        setIsRefetch(true)
+        handleRefetch()
+      } catch (error) {
+        console.log(error.response.data.message)
+        notification.open({
+          message: error.response.data.message,
+          icon: (
+            <WarningOutlined
+              style={{
+                color: "#e91010",
+              }}
+            />
+          ),
+        });
+      }
+    }
+  }
+
+  const EditableCell = ({
+    editing,
+    dataIndex,
+    title,
+    inputType,
+    record,
+    index,
+    children,
+    ...restProps
+  }) => {
+    const inputNode = () => {
+      if (dataIndex === 'academicYear') return <div>{children}</div>
+      else if (['firstClosureDate', 'finalClosureDate'].includes(dataIndex)) return <DatePicker style={{width: 160}} />
+      else return <Input style={{width: 80}}/>
+    }
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item
+            name={dataIndex}
+            style={{
+              margin: 0,
+            }}
+            rules={[
+              {
+                required: true,
+                message: `Please Input ${title}!`,
+              },
+            ]}
+          >
+            {inputNode()}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+
   const [query, setQuery] = useState({
     page: 1,
     limit: 5,
     name: "",
   });
 
-  const [events, setEvents] = useState([]);
-  const [pagination, setPagination] = useState({});
-
-  const navigate = useNavigate();
-
   const fetchEvents = async () => {
+    handleRefetch()
     try {
       const response = await axiosInstance.get("/event", {
         params: query,
@@ -28,6 +204,7 @@ const Event = () => {
     } catch (error) {
       console.error("Error fetching events:", error);
     }
+    setIsRefetch(false)
   };
 
   const onTableChange = (values) => {
@@ -36,7 +213,7 @@ const Event = () => {
 
   useEffect(() => {
     fetchEvents();
-  }, [query]);
+  }, [query, isRefetch]);
 
   const handleDelete = async (id) => {
     try {
@@ -61,26 +238,32 @@ const Event = () => {
       title: "Event Name",
       dataIndex: "eventName",
       key: "eventName",
+      editable: true,
     },
     {
       title: "First Closure Date",
       dataIndex: "firstClosureDate",
       key: "firstClosureDate",
+      editable: true,
     },
     {
       title: "Final Closure Date",
       dataIndex: "finalClosureDate",
       key: "finalClosureDate",
+      editable: true,
     },
     {
       title: "Academic Year",
       dataIndex: "academicYear",
       key: "academicYear",
+      editable: true,
     },
     {
       title: "Faculty",
       dataIndex: "faculty",
       key: "faculty",
+      editable: true,
+
       render: (data) => (
         <div style={{ textTransform: "lowercase" }}>{data}</div>
       ),
@@ -88,24 +271,88 @@ const Event = () => {
     {
       title: "Action",
       key: "action",
-      render: (_, record) => (
-        <div>
-          <Link to={`/event/detail/${record._id}`}>
-            <EyeOutlined />
-          </Link>
-          <Button
-            type="link"
-            onClick={() => navigate(`/user/edit/${record._id}`)}
-          >
-            <EditOutlined style={{ paddingLeft: 12, paddingRight: 12 }} />
-          </Button>
-          <Button type="link" onClick={() => handleDelete(record._id)}>
-            <DeleteOutlined />
-          </Button>
-        </div>
-      ),
+      
+      render: (_, param2, index) => {
+        if (param2?.isCreate || param2?._id === editId) {
+        return (
+          <div>
+            <Button
+              type="link"
+              onClick={() => handleSave(param2?.isCreate, param2._id)}
+            >
+              <CheckOutlined />
+            </Button>
+            <Button
+              type="link"
+              onClick={() => handleRemove(index)}
+            >
+              <CloseOutlined />
+            </Button>
+          </div>
+        )}
+      return (
+          <div>
+            <Link to={`/event/detail/${param2?._id}`} disabled={isCreate}>
+              <EyeOutlined />
+            </Link>
+            <Button
+              type="link"
+              onClick={() => handleEdit(param2)}
+              disabled={isCreate}
+            >
+              <EditOutlined
+                style={{
+                  paddingLeft: 12,
+                  paddingRight: 12,
+                }}
+              />
+            </Button>
+            <Button type="link" onClick={() => handleDelete(record._id)}>
+              <DeleteOutlined />
+            </Button>
+          </div>
+      )},
+    },
+    {
+      title: "Upload",
+      dataIndex: "upload",
+      key: "upload",
+      render: (_, param2) => {
+        if (!param2?.isCreate) {
+          return (
+          <div>
+            <Button
+              type="primary"
+              onClick={() => {
+                navigate(`/event/post/${param2?._id}`);
+              }}
+            >
+              Upload
+            </Button>
+          </div>
+          )}
+        return (
+          <div></div>
+        )
+      },
     },
   ];
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: record._id === editId,
+      }),
+    };
+  });
 
   return (
     <div className="profile" style={{ marginTop: 30, padding: "0 40px" }}>
@@ -133,19 +380,24 @@ const Event = () => {
             </Space>
           </Col>
           <Col span={6} style={{ textAlign: "right" }}>
-            <Button type="primary" onClick={() => navigate("/createuser")}>
-              Create Event
-            </Button>
+          <Button disabled={isCreate || !!editId} onClick={handleCreate}>Create Event</Button>
           </Col>
         </Row>
       </div>
-      <Table
-        dataSource={events}
-        columns={columns}
-        pagination={pagination}
-        onChange={onTableChange}
-        rowKey="_id"
-      />
+      <Form form={form} component={false}>
+        <Table
+          components={{
+            body: {
+              cell: EditableCell,
+            },
+          }}
+          dataSource={events}
+          columns={mergedColumns}
+          pagination={pagination}
+          onChange={onTableChange}
+          rowKey="_id"
+        />
+      </Form>
     </div>
   );
 };
