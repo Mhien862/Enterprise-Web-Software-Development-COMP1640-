6653289bf3bf1cd4e4fe75f47ce24ba19c243f83
Mhien862 <104289231+Mhien862@users.noreply.github.com>
2024-04-23 23:41:17 +0700

    123

--- fe/src/store/index.js
+++ fe/src/store/index.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import profileReducer from './reducers/profile';
+
 import serviceReducer from './reducers/service';
-import managementReducer from './reducers/management';
-import ownerReducer from './reducers/owner';
+// import managementReducer from './reducers/management';
+// import ownerReducer from './reducers/owner';
 
 export const store = configureStore({
   reducer: {
-    profile: profileReducer,
-    management: managementReducer,
+    
+    // management: managementReducer,
     service: serviceReducer,
-    owner: ownerReducer,
+    // owner: ownerReducer,
   },
 });
