f246258a6cebdc7e477b10be1b92ee58561af02f
taylortien-99 <taylortien99@gmail.com>
2024-04-23 20:42:07 +0700

    fix token

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,22 +1,19 @@
 import jwt from "jsonwebtoken";
-import User from "../models/userModels.js";
 const authenticate = async (req, res, next) => {
   const token = req.headers["authorization"];
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select("-password");
-      next();
-    } catch (error) {
-      res.status(401);
-      throw new Error("Not authorized, token failed");
-    }
-  } else {
-    res.status(401);
-    throw new Error("Not authorized, no token");
+  try {
+    // auto verify
+    jwt.verify(token, process.env.JWT_SECRET);
+    // req.user = await User.findById(decoded.userId).select("-password");
+    next();
+  } catch (error) {
+    // not crash app
+    res.send(401);
+    next(error);
   }
 };
+
 const authenticateAdmin = async (req, res, next) => {
   // Kiểm tra xem người dùng đã đăng nhập và có vai trò là admin không
   if (req.user && req.user.role === "admin") {
