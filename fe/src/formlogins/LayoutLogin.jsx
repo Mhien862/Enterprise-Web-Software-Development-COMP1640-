16b8a07241aaae114b20e29959517dea8fd78dae
Mhien862 <104289231+Mhien862@users.noreply.github.com>
2024-04-25 01:53:24 +0700

    123

--- fe/src/formlogins/LayoutLogin.jsx
+++ fe/src/formlogins/LayoutLogin.jsx
@@ -1,44 +1,41 @@
-import { Card } from 'antd';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Card } from "antd";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axiosInstance from "../services/axios.service";
 
 const LayoutLogin = () => {
-  const [isHide, setIsHide] = useState(true);
+  // const [isHide, setIsHide] = useState(true);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    // call API
-    const fetchService = async () => {
-      try {
-        const response = await axiosInstance.get("/profile", {});
-        navigate("/");
-        setIsHide(false);
-      } catch (error) {
-        const response = await axiosInstance.post("/logout", {});
-        setIsHide(false);
-        console.log(error);
-      }
-    };
+  // useEffect(() => {
+  //   // call API
+  //   const fetchService = async () => {
+  //     try {
+  //       const response = await axiosInstance.get("/profile", {});
+  //       navigate("/");
+  //       setIsHide(false);
+  //     } catch (error) {
+  //       const response = await axiosInstance.post("/logout", {});
+  //       setIsHide(false);
+  //       console.log(error);
+  //     }
+  //   };
 
-    fetchService();
-  }, []);
+  //   fetchService();
+  // }, []);
 
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      {isHide ?
-        null :
-        <Card>
-          <Outlet />
-        </Card>
-      }
+      <Card>
+        <Outlet />
+      </Card>
     </div>
   );
 };
