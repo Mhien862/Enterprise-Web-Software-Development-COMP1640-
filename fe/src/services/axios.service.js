9a39592f02e4953497050423bf317de1848bccb2
Mhien862 <104289231+Mhien862@users.noreply.github.com>
2024-04-24 13:46:56 +0700

    123

--- fe/src/services/axios.service.js
+++ fe/src/services/axios.service.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:1000/',
+  //baseURL: 'https://enterprise-web-software-development-yjxw.onrender.com/',
   withCredentials: true
 });
 
